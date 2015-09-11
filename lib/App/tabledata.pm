39a916be6cdfa22fa441d0fb3365f79af3e54510
perlancar (@pc-office) <perlancar@gmail.com>
2015-09-11 19:18:40 +0700

    WIP

--- lib/App/tabledata.pm
+++ lib/App/tabledata.pm
@@ -3,23 +3,9 @@ package App::tabledata;
 # DATE
 # VERSION
 
-use 5.010001;
-use strict;
-use warnings;
-#use experimental 'smartmatch';
-use Log::Any '$log';
-
-use File::chdir;
-
-# for testing
-use Time::HiRes qw(sleep);
-
-#require Exporter;
-#our @ISA       = qw(Exporter);
-#our @EXPORT_OK = qw(rmhere);
-
-our %SPEC;
-
 1;
-# ABSTRACT:
+# ABSTRACT: Manipulate table data
 
+=head1 DESCRIPTION
+
+See the included script L<tabledata>.
